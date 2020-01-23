76ee72e94ffb618bedbd292420073c4ae23d159f
unknown <daniel.alfonso-b@labinfo.is.escuelaing.edu.co>
2020-01-23 10:00:18 -0500

    Falta ultima parte figuras

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -0,0 +1,31 @@
+package edu.eci.cvds.patterns.shapes;
+import edu.eci.cvds.patterns.shapes.concrete.*;
+
+
+public class ShapeFactory {
+
+    public static Shape create(RegularShapeType shapeType) {
+		switch(shapeType){
+			case Triangle:
+				return new Triangle();
+				break;
+				
+			case Pentagon:
+				return new Pentagon();
+				break;
+				
+			case Hexagon:
+				return new Hexagon();
+				break;
+				
+			case Quadrilateral:
+				return new Quadrilateral();
+				break;
+				
+			default: 
+				break;
+		}
+
+    }
+
+}
